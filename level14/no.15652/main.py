ae77113c90077bb1e1d9bb63086cca3964cc6d60
devel-won <develwon@gmail.com>
2021-06-24 19:57:19 +0900

    level14 commit 0624

---
+++ level14/no.15652/main.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def d(in_list, r):
+    for i in range(len(in_list)):
+        if r == 1:
+            yield [in_list[i]]
+        else:
+            tmp_list = in_list.copy()
+            for j in d(tmp_list[i:], r-1):
+                yield [in_list[i]] + j
+
+
+N, M = map(int, sys.stdin.readline().strip().split())
+
+A = [i for i in range(1, N+1)]
+
+result = d(A, M)
+
+for i in d(A, M):
+    for j in i:
+        print(j, end=' ')
+    print()
