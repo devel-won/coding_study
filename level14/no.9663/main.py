03ccd4e206e60fa4d5efe49cc6701cf794b364d0
devel-won <develwon@gmail.com>
2021-06-28 20:31:51 +0900

    level14 commit 0628

--- level14/no.9663/main.py
+++ level14/no.9663/main.py
@@ -1,61 +1,23 @@
-import sys
-
-N = int(sys.stdin.readline().strip())
-
-A = [i for i in range(1, N+1)]
-B = [[1] * N for i in range(N)]
-
-
-count_value = 0
-
-
-def check2(result_list, r):
-    for i in range(1, r):
-        if (result_list[-1] == result_list[-1 - i] - i) or (result_list[-1] == result_list[-1 - i] + i):
+def check(n):
+    for i in range(n):
+        if row[n] == row[i] or abs(row[n] - row[i]) == n - i:
             return 0
     return 1
 
 
-def check(row, col):
-    for i in range(N - col):
-        if i == 0:
-            B[row][col] = 0
-        else:
-            if row + i <= N - 1:
-                B[row + i][col + i] = 0
-            if row - i >= 0:
-                B[row - i][col + i] = 0
+def dfs(n):
+    global res
+    if n == N:
+        res += 1
+    else:
+        for i in range(N):
+            row[n] = i
+            if check(n):
+                dfs(n + 1)
 
 
-def d(in_list, r):
-    global B
-
-    for i in range(len(in_list)):
-        if r == 1:
-            check(in_list[i] - 1, r - 1)
-            yield [in_list[i]]
-        else:
-            tmp_list = in_list.copy()
-            tmp_list.pop(i)
-            for j in d(tmp_list, r-1):
-                if B[in_list[i] - 1][r - 1]:
-                    tmp2_list = j + [in_list[i]]
-                    check(in_list[i] - 1, r - 1)
-                    yield tmp2_list
-                # tmp2_list = j + [in_list[i]]
-                # check(in_list[i] - 1, r - 1)
-                # if check2(tmp2_list, r):
-                #     yield tmp2_list
-                else:
-                    B = [[1] * N for i in range(N)]
-
-
-result = d(A, N)
-
-for i in result:
-    count_value += 1
-    for j in i:
-        print(j, end=' ')
-    print()
-
-print(count_value)
+N = int(input())
+row = [0] * N
+res = 0
+dfs(0)
+print(res)
