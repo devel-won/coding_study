f4766ece555ac93a486ee2ddfc72afb9f512afc9
devel-won <develwon@gmail.com>
2021-07-21 20:21:25 +0900

    level14 commit 0721

--- level14/no.2580/main.py
+++ level14/no.2580/main.py
@@ -2,69 +2,42 @@ import sys
 
 Map = []
 zero_list = []
+n = 0
 row = [[False]*9 for _ in range(9)]
 col = [[False]*9 for _ in range(9)]
 squ = [[False]*9 for _ in range(9)]
 
 for i in range(9):
-    col = list(map(int, sys.stdin.readline().strip().split()))
-    Map.extend(col)
+    col2 = list(map(int, sys.stdin.readline().strip().split()))
+    Map.extend(col2)
 
-    for index, j in enumerate(col):
+    for index, j in enumerate(col2):
         if j == 0:
             zero_list.append([i, index])
+            n += 1
         else:
-            row[i][j] = True
-            col[index][j] = True
+            row[i][j-1] = True
+            col[index][j-1] = True
+            squ[i//3*3+index//3][j-1] = True
 
 
-def check(x, y):
-    A = [i for i in range(1, 10)]
-    for i in range(9):
-        if Map[x*9 + i] in A:
-            A.remove(Map[x*9 + i])
-        if Map[i*9 + y] in A:
-            A.remove(Map[i*9 + y])
-
-    x1 = x//3
-    y1 = y//3
-
-    for i in range(x1 * 3, (x1 + 1) * 3):
-        for j in range(y1 * 3, (y1 + 1) * 3):
-            if Map[i * 9 + j] in A:
-                A.remove(Map[i * 9 + j])
-    return A
-
-
-def dfs():
+def dfs(x):
     global Map
-    if len(zero_list) == 0:
-        return 1
+    if x == n:
+        for s in range(9):
+            print(' '.join(map(str, Map[s * 9:(s + 1) * 9])))
+        exit(0)
 
-    i, j = zero_list[-1]
+    a, b = zero_list[x]
 
-    for s in check(i, j):
-        Map[i * 9 + j] = s
-        del zero_list[-1]
-        if dfs():
-            return 1
-        else:
-            zero_list.append([i, j])
-            Map[i * 9 + j] = 0
-    return 0
+    for k2 in range(1, 10):
+        k = k2 - 1
+        if not(row[a][k] or col[b][k] or squ[a//3*3+b//3][k]):
+            row[a][k] = col[b][k] = squ[a // 3 * 3 + b // 3][k] = True
+            Map[a*9 + b] = k2
+            dfs(x + 1)
+            row[a][k] = col[b][k] = squ[a // 3 * 3 + b // 3][k] = False
+            Map[a * 9 + b] = 0
 
 
-dfs()
-
-for i in range(9):
-    print(' '.join(map(str, Map[i * 9:(i + 1) * 9])))
-
-# 0 0 0 0 0 0 0 0 0
-# 0 0 0 0 0 0 0 0 0
-# 0 0 0 0 0 0 0 0 0
-# 0 0 0 0 0 0 0 0 0
-# 0 0 0 0 0 0 0 0 0
-# 0 0 0 0 0 0 0 0 0
-# 0 0 0 0 0 0 0 0 0
-# 0 0 0 0 0 0 0 0 0
-# 0 0 0 0 0 0 0 0 0
+dfs(0)
