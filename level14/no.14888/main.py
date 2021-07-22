c83a7808f71eb9aee5a15f6591a627e8658c7922
devel-won <develwon@gmail.com>
2021-07-22 18:29:52 +0900

    level14 commit 0722 no.14888 시작

--- level14/no.14888/main.py
+++ level14/no.14888/main.py
@@ -1,26 +1,11 @@
 import sys
 
-Map = []
-for i in range(9):
-    Map.append(list(map(int, sys.stdin.readline().strip().split())))
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().strip().split()))
+# B = 연산자 , + - x / 개수만큼 저장됨
+B = list(map(int, sys.stdin.readline().strip().split()))
 
-
-def check(x, y, num):
-    for i in range(9):
-        if Map[x][i] == num or Map[i][y] == num:
-            return 0
-
-    for i in range(x//3 * 3, (x // 3 + 1) * 3):
-        for j in range((y//3 * 3, (y // 3 + 1) * 3)):
-            if Map[i][j] == num:
-                return 0
-    return 1
-
-
-def dfs(x, y, z):
-    for i in range(9):
-        for j in range(9):
-            if Map[i][j] == 0:
-                pass
-
-print("check")
+# 주어진 숫자 자리는 바꿀 수 없음
+# 순서대로 계산됨
+# 최댓값, 최솟값 구하기
+# / 연산자 결과는 정수만 사용
